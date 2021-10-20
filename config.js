c3e8c37d8b7c1c1f34e588b159c8f28ddc991c05
thinhsa <92832431+thinhsa@users.noreply.github.com>
2021-10-20 08:00:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const CONFIG = {
-    titleWeb: "HÔM NAY LÀ 20/10 ĐẤYYYYY",
+    titleWeb: "HÔM NAY LÀ 20/10 ĐẤYYY",
     introTitle: 'NHÉ CÁC BẠN GÁI LỚP 11 CHUYÊN ANH',
-    introDesc: `Trái đất hình cầu, nhưng 20/10 vẫn là ngày ở đỉnh chóp`,
+    introDesc: `Trái đất hình cầu, nhưng 20/10 vẫn là ngày ở đỉnh chóp
+                Còn vị trí chúng mày thì luôn ở TOPPPPP !`,
     btnIntro: '^^Ahihi^^',
     title: 'Chúc chúng mày 20/10 ngày càng xinh đẹp, giỏi giang, hiền dịu, nết na, vui vẻ, yêu đời, không sầu muộn; thi gì đỗ nấy, mà hỏi lấy thì ai cũng đồng ý =))',
     desc: 'Chúng mày thấy con trai lớp 11 Anh đẹp trai, tuyệt vời, tâm lý hơm?? ',
