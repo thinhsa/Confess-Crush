3edee27a9157809bf3c0a8ef078f9cdcb219a6ab
thinhsa <92832431+thinhsa@users.noreply.github.com>
2021-10-20 16:47:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "HÔM NAY LÀ 20/10 ĐẤYYY",
-    introTitle: 'Nhé các chị em của Page này giúp bạn thúc đẩy quá trình học tiếng anh',
+    introTitle: 'Nhé các bạn nữ xinh đẹp của lớp 11 Anh !',
     introDesc: `Trái đất tròn, lòng tôi góc cạnh
     Ngày bạn đến, lòng tôi bằng phẳng
     Trái đất hình cầu, nhưng 20/10 vẫn là ngày ở đỉnh chóp
-    Còn vị trí chị em thì luôn ở đỉnh TOPPPPP !`,
+    Còn vị trí chúng mày thì luôn ở đỉnh TOPPPPP !`,
     btnIntro: '^^Ahihi^^',
-    title: 'Chúc 20/10 chị em phải luôn xinh đẹp, rạng rỡ, giữ được nụ cười trên môi. Học thì phải giỏi. Thi gì đỗ nấy, 9.0 IELTS, 120 TOEFL giải nhất HSG,....'',
-    desc: 'Hôm nay là ngày của các chị em, mặc dù là còn nhiều phiền muộn cũ, nhưng đọc đến đây thì đã quên hết đi rồi chứ? ',
-    btnYes: 'Đương nhiên là quên hết rồi. Đọc xong làm sao mà không quên được !!',
-    btnNo: 'Không nha, tui vẫn còn buồn lắm :(( Nỗi buồn khum thể vơi',
-    question: 'Page mình ngoài kiến thức ra thì còn có gì nữa dị ??',
-    btnReply: 'Gửi đi với triệu trái tym <3',
-    reply: 'Có tình yêu thưn đong đầy và có những bạn gái cực tuyệt, cực giỏi giang, đa tài, xinh xắn nhứt thiên hạ !!'',
-    mess: 'Hehe tui biết là sẽ nói vậy mà, nhưng mà còn có cả các bạn nam tâm lý nữa nhé ^^',
-    messDesc: 'Nói vậy chứ, 20/10 là ngày dành riêng cho chị em. Mọi người phải vứt bỏ hết deadlines để dành thời gian thư giãn cho bản thân. Các bạn xứng đáng được trân trọng như thế <3. Ấn oke để tui hát cho nghe nè =))',
-    btnAccept: 'Okiiiii lun. Cảm ơn nhiều lắm lun á !!',
-    messLink: 'https://www.youtube.com/watch?v=Ope8MY5zjGA/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Chúc 20/10 chúng mày luôn xinh đẹp, vui tươi, cười nhiều hơn để xua đi nỗi buồn. Thi 9.0 IELTS, giải nhất HSG, 29 điểm ĐH. Sớm có người yêu :))))'',
+    desc: 'Các cậu có thấy con trai lớp 11 Anh tâm lý không nào??,
+    btnYes: 'Vừa tâm lý lại còn đẹp trai, ga lăng, tuyệt vời nhất hệ mặt trời!',
+    btnNo: 'Không nha, ở đấy mà mơ. Con trai lớp Anh chẳng ga lăng tí nào cả :>',
+    question: 'Các cậu có muốn gửi cho 7 bạn con trai điều gì muốn nói không nè??',
+    btnReply: 'Gửi cho 7 người đàn ông 11 chuyên Anh <3',
+    reply: 'Các bạn con trai lớp mình tâm lý ghê á. Cảm ơn vì đã chúc bọn mình 20/10 nha. Các cậu xứng đáng 10 người yêu'',
+    mess: 'Bọn tao cảm ơn nha, biết là chúng mày sẽ nói thế mà hihi 😘😘',
+    messDesc: 'Nói vậy chứ, 20/10 phải thật vui vẻ nhé, thỏa sức mà chơi, bỏ lại deadlines và XÕA HẾT MÌNH đó nha!! Hôm nay là ngày của chúng mày. Chúng mày là những công chúa lộng lẫy nhất không chỉ hôm nay mà còn là 364 ngày còn lại nữa. Bấm Oke để xem những chân tình của các bạn nam nha:',
+    btnAccept: 'Okiiiii lun. Để tao chờ đợi xem chúng mày làm gì nha!!',
+    messLink: 'https://youtu.be/8McC1UtG0H4/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
